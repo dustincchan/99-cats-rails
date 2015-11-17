bddc1d47f05a0144193f536ac22db7b75cca21f3
Winnie Ngo <ngowinn@gmail.com>
2015-11-17 14:43:59 -0800

    add create, new methods to cat_controller and cat views

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,10 +1,30 @@
 class CatsController < ApplicationController
   def index
+    @cats = Cat.all
+    render :index
   end
 
   def show
+    @cat = Cat.find(params[:id])
+    render :show
   end
 
   def new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :sex, :color, :birth_date, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "cats#index"
-  resources :cats, only:[:index, :show, :new]
+  resources :cats, only:[:index, :show, :new, :create]
 end
